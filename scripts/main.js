616844f105a7a2fd8909949d16b3b619f546aada
Anna <ana84168@gmail.com>
2021-01-02 13:35:36 -0500

    Update 02012021

---
+++ scripts/main.js
@@ -0,0 +1,129 @@
+//DROPDOWN
+//Propiedades y Metodos del objeto
+function Dropdown(options){
+    const {
+        elem
+    } = options;
+    const element = document.querySelector(elem);
+    //Ejecutar evento
+    this.addEvent(element);
+}
+//Evento
+Dropdown.prototype.addEvent = function(element){
+    element.onclick = () => {
+        element.classList.toggle('open_dropdown');
+    }
+}
+
+//Creo el objeto
+const dropdownButton = new Dropdown({
+    elem: '.js_dropdown'
+})
+const dropdownButton2 = new Dropdown({
+    elem: '.js_dropdown2'
+})
+///////////////////////////////////////////////////////////////////
+//ACCORDION
+// function AccordionB(options){
+//     this.element = document.querySelector(options.element);
+//     this.accordionItems = this.element.querySelectorAll('.js_accordion_item h3');
+
+//     this.toggleAccordion(options);
+//     this.openAccordion();
+// }
+
+// AccordionB.prototype.openAccordion = function(){
+//     const itemToggle = this.accordionItems.getAttribute('aria-expanded');
+//     console.log(itemToggle);
+// }
+
+// //Evento
+// AccordionB.prototype.toggleAccordion = function(options){
+//     const array = this.accordionItems;
+//     array.forEach(element => {
+//         console.log('Hola');
+//     });
+//         console.log(array);
+// }
+
+// const accordion = new AccordionB({
+//     element: '.js_accordion'
+// })
+///////////////////////////////////////////////////////////////////////
+const items = document.querySelectorAll(".js_accordion h3");
+
+function toggleAccordion() {
+  const itemToggle = this.parentNode.getAttribute('area-expanded');
+  
+  for (i = 0; i < items.length; i++) {
+    items[i].parentNode.setAttribute('area-expanded', 'false');
+  }
+  
+  if (itemToggle == 'false') {
+    this.parentNode.setAttribute('area-expanded', 'true');
+  }
+}
+
+items.forEach(item => item.addEventListener('click', toggleAccordion));
+
+/////////////////////////////////////////////////////////////////////
+//MODAL
+function Modal(options){
+    this.element = document.querySelector(options.element);
+    this.elementClose = this.element.querySelector('.js_close');
+    this.elementAccept = this.element.querySelector('.js_accept');
+    this.elementCancel = this.element.querySelector('.js_cancel');
+
+    this.registerEvens(options);
+}
+
+Modal.prototype.open = function(){
+    this.element.classList.add('is-active');
+}
+Modal.prototype.close = function(){
+    this.element.classList.remove('is-active');
+}
+Modal.prototype.registerEvens = function(options){
+    //Se usa arrow function en este caso para que el this.close apunte 
+    //a la funcion
+    this.elementClose.onclick = () =>{
+        this.close();
+    }
+    this.elementAccept.onclick = options.cbAccept;
+    this.elementCancel.onclick = options.cbCancel;
+
+}
+
+//Crear objetos
+const modal1 = new Modal({
+    element: '.js_modal1',
+    cbAccept: function(){
+        console.log('aceptar');
+        modal1.close();
+    },
+    cbCancel: function(){
+        console.log('cancelar');
+        modal1.close();
+    }
+})
+
+const modal2 = new Modal({
+    element: '.js_modal2',
+    cbAccept: function(){
+        console.log('aceptar');
+        modal2.close();
+    },
+    cbCancel: function(){
+        console.log('cancelar');
+        modal2.close();
+    }
+})
+
+const button1 = document.querySelector('.js_buttonModal1');
+button1.onclick = function(){
+    modal1.open();
+}
+const button2 = document.querySelector('.js_buttonModal2');
+button2.onclick = function(){
+    modal2.open();
+}
